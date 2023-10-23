7d322c008fb98577fd110475bcfefde801e6b7da
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-10-22 23:43:45 -0400

    Deleted test images

--- components/Header/index.tsx
+++ components/Header/index.tsx
@@ -1,6 +1,6 @@
 export default function Header() {
   return (
-    <header className="w-full h-screen bg-cover bg-center bg-[url('/image/people3.jpg')]">
+    <header className="w-full h-screen bg-cover bg-center bg-[url('/image/people.jpg')]">
       <div className="flex justify-center items-center container mx-auto px-6 h-screen">
         <div className="bg-[rgba(0,0,0,0.3)] rounded-lg">
           <span className="text-white text-center font-normal text-3xl block mt-10 overflow-y-hidden"><i>Hola, yo soy</i></span>
