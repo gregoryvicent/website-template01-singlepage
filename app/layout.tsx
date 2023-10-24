0039b7baae6b5bd3fe0b093d9c73f89ddac2a5ff
Gregory Vicent <gregoryvicent.dev@gmail.com>
2023-10-24 09:04:48 -0400

    New comments in app/page.tsx and app/layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,22 +1,30 @@
+// Importamos el tipo Metadata de 'next'
 import type { Metadata } from 'next'
+// Importamos la fuente Inter de 'next/font/google'
 import { Inter } from 'next/font/google'
+// Importamos los estilos globales
 import './globals.css'
 
+// Creamos una instancia de la fuente Inter con el subconjunto 'latin'
 const inter = Inter({ subsets: ['latin'] })
 
+// Exportamos los metadatos de la aplicación
 export const metadata: Metadata = {
-  title: 'LaraTech',
-  description: 'LaraTech Portfolio',
+  title: 'LaraTech', // Título de la aplicación
+  description: 'LaraTech Portfolio', // Descripción de la aplicación
 }
 
+// Exportamos el componente RootLayout por defecto
 export default function RootLayout({
-  children,
+  children, // Propiedad children que representa los componentos hijos
 }: {
-  children: React.ReactNode
+  children: React.ReactNode // Definimos el tipo de children como ReactNode
 }) {
   return (
+    // Devolvemos un elemento html con el atributo lang establecido en 'es'
     <html lang="es">
-      <body className={inter.className}>{children}</body>
+      {/* // Dentro del elemento html, tenemos un elemento body con la clase de la fuente Inter */}
+      <body className={inter.className}>{children}</body> {/* Renderizamos los componentes hijos dentro del body */}
     </html>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,15 @@
+// Importamos el componente Image de 'next/image'
 import Image from 'next/image'
 
+// Importamos el componente Header de '../components/Header'
 import Header from '../components/Header'
 
+// Exportamos la función Home como el componente predeterminado
 export default function Home() {
   return (
+    // El componente principal que se devuelve es un elemento <main>
     <main className="">
+      {/* Dentro del elemento <main>, insertamos el componente Header */}
       <Header />
     </main>
   )
